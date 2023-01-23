152932821180d8cc10fbf90f600020b20cc5cdbd
MikhailKostromin <110930748+MikhailKostromin@users.noreply.github.com>
2023-01-23 23:07:30 +0300

    Delete src/HW_2_OOP_JAVA directory

--- src/HW_2_OOP_JAVA/Search.java
+++
@@ -1,8 +0,0 @@
-package HW_2_OOP_JAVA;
-
-public interface Search {
-    String getFamilyName();
-    void findRelative(); // поиск родственников
-    int getAge();
-    
-}
--- src/HW_2_OOP_JAVA/Search2.java
+++
@@ -1,5 +0,0 @@
-package HW_2_OOP_JAVA;
-
-public interface Search2 {
-    void findRelative2();
-}
