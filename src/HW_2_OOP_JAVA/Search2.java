22e1ae035c73425bd4b5585c16d9159b4eb3d158
mihailkostromin <kostrominmi@mail.ru>
2023-01-23 23:00:14 +0300

    add

---
+++ src/HW_2_OOP_JAVA/Search2.java
@@ -0,0 +1,5 @@
+package HW_2_OOP_JAVA;
+
+public interface Search2 {
+    void findRelative2();
+}
---
+++ src/HW_2_OOP_JAVA/Test1.java
@@ -0,0 +1,29 @@
+package HW_2_OOP_JAVA;
+
+import HW_1_OOP_JAVA.Family;
+
+public class Test1 implements Search, Search2 {
+    String familyName;
+    int age;
+
+    @Override
+    public String getFamilyName() {
+        if (familyName == familyName) return "Родственник";
+        else return " НЕ Родственник";
+    }
+
+    @Override
+    public void findRelative() {
+        System.out.println(familyName);
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void findRelative2() {
+        System.out.println(familyName);
+    }
+}
